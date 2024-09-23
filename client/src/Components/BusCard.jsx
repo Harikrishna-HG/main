a4b3e7fbd8d84d7abcf04948a56297ab5a3b1015
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2024-09-24 00:26:51 +0545

    Update BusCard.jsx

--- client/src/Components/BusCard.jsx
+++ client/src/Components/BusCard.jsx
@@ -8,7 +8,7 @@ const BusCard = () => {
   useEffect(() => {
     const fetchBuses = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/bus", {
+        const response = await fetch("https://main-yxw3.onrender.com", {
           method: "GET",
         });
         if (!response.ok) {
