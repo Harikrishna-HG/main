ba62c873c474bf2355e05a1255d9711c9b0bad25
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2024-09-24 00:28:51 +0545

    Update Login.jsx

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -30,7 +30,7 @@ const Login = ({ isDarkMode }) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      const response = await fetch(`https://main-yxw3.onrender.com`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
